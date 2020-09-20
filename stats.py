38db73504a66ed392efd7ffe9f3f7b7cd313171d
Alex Lagoda <lagoda.w1@gmail.com>
2020-09-20 20:52:06 +0300

    Stats respect the IS/OS

--- stats.py
+++ stats.py
@@ -14,9 +14,14 @@ import numpy as np
 # 7:BET_NUMBER
 
 
-def calculate_pnl_stats(pnl_file):
+def calculate_pnl_stats(pnl_file, start_date=None, end_date=None):
     result = dict()
-    pnl_data = np.loadtxt(pnl_file)
+    full_pnl = np.loadtxt(pnl_file)
+    pnl_data = full_pnl
+    if start_date:
+        pnl_data = pnl_data[pnl_data[:,0] >= start_date]
+    if end_date:
+        pnl_data = pnl_data[pnl_data[:,0] < end_date]
     result['CumPNL'] = pnl_data.sum(axis=0)[1]
     result['Total_costs'] = pnl_data.sum(axis=0)[3]
     result['avgBet'] = pnl_data.mean(axis=0)[7]
@@ -26,3 +31,9 @@ def calculate_pnl_stats(pnl_file):
     result['Return'] = ((1. + result['TotalReturn']) ** (1. / (result['tradeDays'] / 252.))) - 1.
     result['Sharpe'] = pnl_data.mean(axis=0)[1] / np.std(pnl_data, axis=0)[1] * math.sqrt(252)
     return result
+
+
+def cross_validate(pnl_file, is_start, os_start, end=None):
+    is_stats = calculate_pnl_stats(pnl_file, start_date=is_start, end_date=os_start)
+    os_stats = calculate_pnl_stats(pnl_file, start_date=os_start, end_date=end)
+    return {'IS': is_stats, 'OS': os_stats, 'OSIS': os_stats['Sharpe'] / is_stats['Sharpe']}
